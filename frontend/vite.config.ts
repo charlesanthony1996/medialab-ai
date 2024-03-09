6a2023624f22b92bbfd439318d3f3a3b1c6eada8
Charles Anthony <anthony@cidanmachinery.com>
2024-03-09 17:24:43 +0100

    extension changes with manifest.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000
+  },
   plugins: [vue()],
-})
+  build: {
+    outDir: 'public', // Ensure this points to your extension's directory
+  },
+});
