42d1ddf05f1939554d40d07c71d755ad7a447a5f
Charles Anthony <anthony@cidanmachinery.com>
2024-03-14 11:59:00 +0100

    minor changes

--- frontend/src/components/HateSpeech.vue
+++ frontend/src/components/HateSpeech.vue
@@ -1,14 +1,48 @@
 <template>
-    <p>Detecting hate speech</p>
-    
+    <p>Trying to detect hate speech</p>
+    <p>Your url: {{ url_name }}</p>
+    <v-btn to="" variant="outlined" @click="getComments">Get Comment</v-btn>
+    <p style="">display comments: {{ comment_des }}</p>
 </template>
 
 
 
-<script>
-import { ref } from 'vue'
+<script setup>
+import { ref, computed } from 'vue'
+import axios from 'axios'
+import { getCurrentTab, url_name } from '../services/services'
 
-const testTag = ref('')
+
+const comment_des = ref('')
+
+function testFunction() {
+    console.log("test function")
+}
+
+const getComments = async () => {
+    try{
+        const response = await axios.get("http://localhost:8000/api/comments")
+        comment_des = response.data.comment
+        console.log(comment_des)
+    }
+    catch(error) {
+        console.log(error)
+    }
+}
+
+const isYoutube = computed(() => {
+    if(url_name === "https://www.youtube.com") {
+        // write function to start web scraper. trigger function
+        getComments().then(() => {
+            console.log("done")
+        })
+
+        
+    } else {
+        console.log("couldnt connect to server")
+    }
+})
+
 
 
 </script>
--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,9 +1,11 @@
 <template>
     <p>Sign up here</p>
-    <v-text></v-text>
+    <v-text>name</v-text>
     <v-text-field label=""></v-text-field>
-
-
+    <v-text>email</v-text>
+    <v-text-field label="email"></v-text-field>
+    <v-text>password</v-text>
+    <v-text-field label="password"></v-text-field>
 </template>
 
 
