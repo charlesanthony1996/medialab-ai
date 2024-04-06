e9ca7481b7cbeaa55898fb69810738ebcc8d2bae
Charles Anthony <anthony@cidanmachinery.com>
2024-04-06 22:17:17 +0200

    added firebase stuff for registration

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,15 +1,36 @@
 <template>
     <p>Sign up here</p>
-    <v-text>name</v-text>
-    <v-text-field label=""></v-text-field>
-    <v-text>email</v-text>
-    <v-text-field label="email"></v-text-field>
-    <v-text>password</v-text>
-    <v-text-field label="password"></v-text-field>
+    <v-text>Name</v-text>
+    <v-text-field label="Name" v-model="name"></v-text-field>
+    <v-text>Email</v-text>
+    <v-text-field label="Email" v-model="email"></v-text-field>
+    <v-text>Password</v-text>
+    <v-text-field label="Password" v-model="password"></v-text-field>
 </template>
 
 
 <script>
+import { ref, computed } from 'vue'
+import firebase from "firebase/compat/app"
+import { useRouter } from 'vue-router'
+
+const email = ref("")
+const password = ref("")
+const router = useRouter()
+
+const register = () => {
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email.value, password.value)
+    .then((data) => {
+        console.log("succesfully registered")
+        router.push("/about")
+    })
+    .catch(error => {
+        console.log(error.code)
+        alert(error.message)
+    })
+}
 
 
 
