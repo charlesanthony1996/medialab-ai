5ba5c94f6779f73c3c64d1f27e7e7a9496001ce2
Charles Anthony <anthony@cidanmachinery.com>
2024-04-06 21:05:04 +0200

    adding some firebase stuff

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,11 +8,34 @@ import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 // import { createRouter } from 'vue-router'
 import router from './router'
+// firebase stuff
+import firebase from "firebase/compat/app"
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
 
 
 const pinia = createPinia()
 const app = createApp(App)
 // const router = VueRouter.createRouter()
+
+var firebaseConfig = {
+
+  apiKey: "AIzaSyCCbLKyEsJjKrxDbAPNW_dO341ph1T7-iI",
+
+  authDomain: "vue-firebase-automation-d374e.firebaseapp.com",
+
+  projectId: "vue-firebase-automation-d374e",
+
+  storageBucket: "vue-firebase-automation-d374e.appspot.com",
+
+  messagingSenderId: "268727744532",
+
+  appId: "1:268727744532:web:09be67565a89d6170b8f01"
+
+};
+
+// firebase initialization
+firebase.initializeApp(firebaseConfig);
 
 const vuetify = createVuetify({
     components,
