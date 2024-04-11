cbbae073e2cac4fbff877d98980a50bc3f27f99c
Charles Anthony <anthony@cidanmachinery.com>
2024-04-11 12:46:59 +0200

    minor changes to nginx.conf file and docker compose.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,10 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 5000
+    port: 8080,
+    host: true,
   },
+  // base: '/hs-extension-app/', 
   plugins: [vue()],
   build: {
     outDir: 'dist', 
