2f93808ab66a9456c08cae1e88cf8c9339d987fa
Charles Anthony <anthony@cidanmachinery.com>
2024-05-02 12:03:09 +0200

    changed popup name to Popup

--- filter/filter.py
+++ filter/filter.py
@@ -1,9 +1,9 @@
 import torch
 import transformers
 import numpy as np
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import json
-import tensorflow as tf
+# import tensorflow as tf
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from scipy.special import softmax
@@ -13,36 +13,66 @@ import os
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
+# class TextProcessor:
+#     def __init__(self, config_path):
+#         with open("config.json", 'r') as config_file:
+#             self.config = json.load(config_file)
+
+#         self.active_config = self.config["models"][self.config["active_model"]]
+#         self.model = TFAutoModelForSequenceClassification.from_pretrained(self.active_config["model_path"])
+#         self.tokenizer = AutoTokenizer.from_pretrained(self.active_config["model_path"])
+#         self.threshold = self.active_config["threshold"]
+
+
+#     def get_current_filter(self):
+#         return self.config["active_model"]
+
+
+#     def preprocess(self, text):
+#         if self.active_config["preprocess"]["lower_case"]:
+#             text = text.lower()
+
+#         if self.active_config["preprocess"]["remove_punctuation"]:
+#             text = ''.join(char for char in text if char.isalnum() or char.isspace())
+#         return text
+
+
+#     def process_text(self, text):
+#         text = self.preprocess(text)
+#         encoded_input = self.tokenizer(text, return_tensors='tf')
+#         output = self.model(encoded_input)
+#         logits = output.logits
+#         scores = tf.nn.softmax(logits, axis=-1).numpy()
+#         negativity_score = scores[0][0]
+#         return negativity_score > self.threshold, text, negativity_score
+
 class TextProcessor:
     def __init__(self, config_path):
-        with open("config.json", 'r') as config_file:
+        with open(config_path, 'r') as config_file:
             self.config = json.load(config_file)
 
         self.active_config = self.config["models"][self.config["active_model"]]
-        self.model = TFAutoModelForSequenceClassification.from_pretrained(self.active_config["model_path"])
+        self.model = AutoModelForSequenceClassification.from_pretrained(self.active_config["model_path"])
         self.tokenizer = AutoTokenizer.from_pretrained(self.active_config["model_path"])
         self.threshold = self.active_config["threshold"]
-
 
     def get_current_filter(self):
         return self.config["active_model"]
 
-
     def preprocess(self, text):
         if self.active_config["preprocess"]["lower_case"]:
             text = text.lower()
-
         if self.active_config["preprocess"]["remove_punctuation"]:
             text = ''.join(char for char in text if char.isalnum() or char.isspace())
         return text
 
-
     def process_text(self, text):
         text = self.preprocess(text)
-        encoded_input = self.tokenizer(text, return_tensors='tf')
-        output = self.model(encoded_input)
+        encoded_input = self.tokenizer(text, return_tensors='pt')
+        with torch.no_grad():
+            output = self.model(**encoded_input)
         logits = output.logits
-        scores = tf.nn.softmax(logits, axis=-1).numpy()
+        scores = torch.nn.functional.softmax(logits, dim=-1).numpy()
         negativity_score = scores[0][0]
         return negativity_score > self.threshold, text, negativity_score
 
